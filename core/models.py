a8cb82d5a9e5edb6f407472c37c417c7941e0bc9
TaylorHomes <taylor.homes01@gmail.com>
2018-12-12 19:28:18 -0500

    add templates and create basic models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Follow(models.Model):
+    following_user = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name="follows_from")
+    followed_user = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name="follows_to")
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    voted_users = models.ManyToManyField(
+        to=User, through='Vote', related_name='user_votes')
+    score = models.IntegerField(default=0)
+
+
+class Vote(models.Model):
+    user = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name="vote")
+    post = models.ForeignKey(
+        to=Post, on_delete=models.SET_NULL, null=True, related_name="votes")
